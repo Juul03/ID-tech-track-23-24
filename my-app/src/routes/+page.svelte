bf5bbebb55cd26543537d600f62dc94c1248a1a4
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-10-31 15:03:11 +0100

    testbarchart_and_styling_added

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit kaas</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+	import TestBarchart from '../components/test.svelte';
+</script>
+
+<TestBarchart/>
+
+<svelte:head>
+	<title>Tech Track Boilerplate</title>
+	<link rel="icon" href="/images/favicon.png" type="image/png" />
+	<link rel="stylesheet" href="generalstyle.css" />
+</svelte:head>
