bf1af20e9e989e106271cb7e91498fb1e33bd27f
Juul03 <118166691+Juul03@users.noreply.github.com>
2023-11-28 18:38:26 +0100

    scss_config_added

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -2,37 +2,42 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 const svelteConfig = {
-  preprocess: preprocess({
-    scss: {
-      // Your SCSS/SASS options go here
-      // For example:
-      // includePaths: ['src', 'node_modules'],
-    },
-    // Other preprocessors or options can be added here if needed
-  }),
-  // Other configurations for SvelteKit can be added here
+	preprocess: preprocess({
+		scss: {
+      includePaths: [
+        'src/components',
+        'static',
+        'node_modules'
+      ],
+      sourceMap: true,
+      outputStyle: 'compressed',
+      autoprefixer: true,
+		}
+		// Other preprocessors or options can be added here if needed
+	})
+	// Other configurations for SvelteKit can be added here
 };
 
 const adapterConfig = {
-  kit: {
-    adapter: adapter({
-      // Inlclude the output.json file in the build
-      // If you want to include the output.json file
-      assets: ['src/data/*'],
+	kit: {
+		adapter: adapter({
+			// Inlclude the output.json file in the build
+			// If you want to include the output.json file
+			assets: ['src/data/*']
 
-      // You can also include entire directories using glob patterns
-      // assets: ['src/data/*'],
+			// You can also include entire directories using glob patterns
+			// assets: ['src/data/*'],
 
-      // Ensure that the file or directory paths are correct based on your project structure
-    }),
-    // Other SvelteKit adapter configurations if needed
-  },
+			// Ensure that the file or directory paths are correct based on your project structure
+		})
+		// Other SvelteKit adapter configurations if needed
+	}
 };
 
 export default {
-  ...svelteConfig, // Spread the Svelte preprocess configuration
-  kit: {
-    ...adapterConfig.kit, // Spread the adapter configuration under 'kit'
-    // Other SvelteKit configurations can be added here if needed
-  },
+	...svelteConfig, // Spread the Svelte preprocess configuration
+	kit: {
+		...adapterConfig.kit // Spread the adapter configuration under 'kit'
+		// Other SvelteKit configurations can be added here if needed
+	}
 };
